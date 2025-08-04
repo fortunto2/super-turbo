9deefc3793646f368ba53c0e0b3c1c931c1923ac
PranovAdilet <pranov.adiletqwe@mail.ru>
2025-08-04 20:04:03 +0600

    Закомментированы части кода в конфигурации Sentry и отключена экспериментальная функция ppr для упрощения отладки и улучшения читаемости. Основная логика осталась без изменений.

--- apps/super-chatbot/next.config.ts
+++ apps/super-chatbot/next.config.ts
@@ -2,9 +2,9 @@ import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    ppr: true,
-  },
+  // experimental: {
+  //   ppr: true,
+  // },
   images: {
     remotePatterns: [
       {
@@ -17,34 +17,37 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+// Временно отключаем Sentry для диагностики проблемы с middleware
+export default nextConfig;
 
-  org: "superduperai",
-  project: "super-chat",
+// export default withSentryConfig(nextConfig, {
+//   // For all available options, see:
+//   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
+//   org: "superduperai",
+//   project: "super-chat",
 
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+//   // Only print logs for uploading source maps in CI
+//   silent: !process.env.CI,
 
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
+//   // For all available options, see:
+//   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  tunnelRoute: "/monitoring",
+//   // Upload a larger set of source maps for prettier stack traces (increases build time)
+//   widenClientFileUpload: true,
 
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
+//   // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+//   // This can increase your server load as well as your hosting bill.
+//   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+//   // side errors will fail.
+//   tunnelRoute: "/monitoring",
 
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
-});
+//   // Automatically tree-shake Sentry logger statements to reduce bundle size
+//   disableLogger: true,
+
+//   // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+//   // See the following for more information:
+//   // https://docs.sentry.io/product/crons/
+//   // https://vercel.com/docs/cron-jobs
+//   automaticVercelMonitors: true,
+// });
--- apps/super-chatbot/src/app/(chat)/layout.tsx
+++ apps/super-chatbot/src/app/(chat)/layout.tsx
@@ -5,7 +5,7 @@ import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { auth } from "../(auth)/auth";
 import Script from "next/script";
 
-export const experimental_ppr = true;
+// export const experimental_ppr = true;
 
 export default async function Layout({
   children,
